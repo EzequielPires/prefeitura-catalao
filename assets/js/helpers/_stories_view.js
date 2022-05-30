06dcc33b69dc617ea1a2e2eef5cf31671604d244
EzequielPires <ezequiel.pires082000@gmail.com>
2022-05-30 07:52:46 -0300

    yasmim gostosa

--- assets/js/helpers/_stories_view.js
+++ assets/js/helpers/_stories_view.js
@@ -2,11 +2,17 @@ import Swiper, {Autoplay, EffectCube, EffectFade, Navigation, Pagination} from '
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+let story = document.querySelector('.story');
+let stories_buttons = document.querySelectorAll('.story-item');
+let btn_close = document.querySelector('.story-close');
 
 const story_view = new Swiper(".story-view", {
     modules: [Navigation, Pagination, EffectCube],
     effect: "cube",
-    grabCursor: true,
+    navigation: {
+        nextEl: ".swiper-button-next-story",
+        prevEl: ".swiper-button-prev-story",
+    },
 });
 const story_view_gallery = new Swiper(".story-view-gallery", {
     modules: [Navigation, Pagination, EffectFade, Autoplay],
@@ -19,28 +25,14 @@ const story_view_gallery = new Swiper(".story-view-gallery", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
-    on: {
-        reachEnd: function () {
-            setTimeout(() => {
-                story_view.slideNext(300);
-            }, 2500)
-        },
-        slideChange: function () {
-            let story_view_active = story_view.activeIndex;
-            let story_view_list = story_view.slides;
-            console.log(story_view_active)
-            story_view_list[story_view_active].contains(this.el) ?
-            setTimeout(() => {
-                this.slideNext(300);
-            }, 2500) : null;
-        },
-        afterInit: function () {
-            let story_view_active = story_view.activeIndex;
-            let story_view_list = story_view.slides;
-            story_view_list[story_view_active].contains(this.el) ?
-            setTimeout(() => {
-                this.slideNext(300);
-            }, 2500) : null;
-        }
-    }
 });
+window.addEventListener('load', ()  => {
+    btn_close.addEventListener('click', () => {
+        story.classList.remove('show');
+    })
+    stories_buttons.forEach(item => {
+        item.addEventListener('click', () => {
+            story.classList.add('show');
+        })
+    })
+})
